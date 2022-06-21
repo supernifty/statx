b7b37de63fe1b1357c1fa4a610350dfbed8e370c
Peter Georgeson <peter.georgeson@gmail.com>
2022-06-21 03:29:19 -0700

    improved anova and pearson

--- statx/anova.py
+++ statx/anova.py
@@ -9,53 +9,82 @@ import sys
 
 import scipy.stats
 
-def anova(values1, values2, values3, values4, values5):
-  logging.info('starting: %i values vs %i...', len(values1), len(values2))
-  logging.debug('group 1: %s; group 2: %s...', ' '.join(values1), ' '.join(values2))
+import statx.t_test
 
-  if len(values1) == 0 or len(values2) == 0:
-    logging.fatal('at least one empty group')
-    return
+def anova(num1, num2, num3=None, num4=None, num5=None, num6=None, num7=None, out_fh=None):
+  logging.info('starting: %i num vs %i...', len(num1), len(num2))
 
-  num1 = [float(x) for x in values1]
-  num2 = [float(x) for x in values2]
+  if len(num1) == 0 or len(num2) == 0:
+    logging.warn('at least one empty group')
+    return {'pvalue': 1, 'statistic': 0}
 
-  if values3 is None:
-    result = scipy.stats.f_oneway(num1, num2)
+  result = {}
+  result['t_test_num1_num2'] = statx.t_test.t_test(num1, num2)['p-value']
+
+  if num3 is None:
+    anova_result = scipy.stats.f_oneway(num1, num2)
   else:
-    num3 = [float(x) for x in values3]
-    if values4 is None:
-      result = scipy.stats.f_oneway(num1, num2, num3)
+    result['t_test_num1_num3'] = statx.t_test.t_test(num1, num3)['p-value']
+    result['t_test_num2_num3'] = statx.t_test.t_test(num2, num3)['p-value']
+    if num4 is None:
+      anova_result = scipy.stats.f_oneway(num1, num2, num3)
     else: 
-      logging.debug('group 4: %s', ' '.join(values4))
-      num4 = [float(x) for x in values4]
-      if values5 is None:
-        result = scipy.stats.f_oneway(num1, num2, num3, num4)
+      result['t_test_num1_num4'] = statx.t_test.t_test(num1, num4)['p-value']
+      result['t_test_num2_num4'] = statx.t_test.t_test(num2, num4)['p-value']
+      result['t_test_num3_num4'] = statx.t_test.t_test(num3, num4)['p-value']
+      if num5 is None:
+        anova_result = scipy.stats.f_oneway(num1, num2, num3, num4)
       else:
-        logging.debug('group 5: %s', ' '.join(values5))
-        num5 = [float(x) for x in values5]
-        result = scipy.stats.f_oneway(num1, num2, num3, num4, num5)
+        result['t_test_num1_num5'] = statx.t_test.t_test(num1, num5)['p-value']
+        result['t_test_num2_num5'] = statx.t_test.t_test(num2, num5)['p-value']
+        result['t_test_num3_num5'] = statx.t_test.t_test(num3, num5)['p-value']
+        result['t_test_num4_num5'] = statx.t_test.t_test(num4, num5)['p-value']
+        if num6 is None:
+          anova_result = scipy.stats.f_oneway(num1, num2, num3, num4, num5)
+        else:
+          result['t_test_num1_num6'] = statx.t_test.t_test(num1, num6)['p-value']
+          result['t_test_num2_num6'] = statx.t_test.t_test(num2, num6)['p-value']
+          result['t_test_num3_num6'] = statx.t_test.t_test(num3, num6)['p-value']
+          result['t_test_num4_num6'] = statx.t_test.t_test(num4, num6)['p-value']
+          result['t_test_num5_num6'] = statx.t_test.t_test(num5, num6)['p-value']
+          if num7 is None:
+            anova_result = scipy.stats.f_oneway(num1, num2, num3, num4, num5, num6)
+          else:
+            result['t_test_num1_num7'] = statx.t_test.t_test(num1, num7)['p-value']
+            result['t_test_num2_num7'] = statx.t_test.t_test(num2, num7)['p-value']
+            result['t_test_num3_num7'] = statx.t_test.t_test(num3, num7)['p-value']
+            result['t_test_num4_num7'] = statx.t_test.t_test(num4, num7)['p-value']
+            result['t_test_num5_num7'] = statx.t_test.t_test(num5, num7)['p-value']
+            result['t_test_num6_num7'] = statx.t_test.t_test(num6, num7)['p-value']
+            anova_result = scipy.stats.f_oneway(num1, num2, num3, num4, num5, num6, num7)
 
-  sys.stdout.write('statistic\tp-value\n')
+  if out_fh is not None:
+    out_fh.write('statistic\tp-value\n')
 
-  if all([num1[0] == x for x in num1 + num2]):
-    logging.info('all values equal')
-    sys.stdout.write('0\t1\n')
-    return
+  #if all([num1[0] == x for x in num1 + num2]):
+  #  logging.info('all values equal')
+  #  if out_fh is not None:
+  #    out_fh.write('0\t1\n')
+  #  return {'pvalue': 1, 'statistic': 0}
 
-  logging.debug(result)
+  result['statistic'] = anova_result.statistic
+  result['pvalue'] = anova_result.pvalue
 
-  sys.stdout.write('{}\t{}\n'.format(result.statistic, result.pvalue))
+  if out_fh is not None:
+    out_fh.write('{}\t{}\n'.format(result['statistic'], result['pvalue']))
 
   logging.info('done')
+  return result
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Perform an ANOVA test')
-  parser.add_argument('--values1', required=True, nargs='+', help='group 1')
-  parser.add_argument('--values2', required=True, nargs='+', help='group 2')
-  parser.add_argument('--values3', required=False, nargs='+', help='group 3')
-  parser.add_argument('--values4', required=False, nargs='+', help='group 4')
-  parser.add_argument('--values5', required=False, nargs='+', help='group 5')
+  parser.add_argument('--values1', required=True, nargs='+', type=float, help='group 1')
+  parser.add_argument('--values2', required=True, nargs='+', type=float, help='group 2')
+  parser.add_argument('--values3', required=False, nargs='+', type=float, help='group 3')
+  parser.add_argument('--values4', required=False, nargs='+', type=float, help='group 4')
+  parser.add_argument('--values5', required=False, nargs='+', type=float, help='group 5')
+  parser.add_argument('--values6', required=False, nargs='+', type=float, help='group 6')
+  parser.add_argument('--values7', required=False, nargs='+', type=float, help='group 7')
   parser.add_argument('--verbose', action='store_true', help='more logging')
   args = parser.parse_args()
   if args.verbose:
@@ -63,5 +92,5 @@ if __name__ == '__main__':
   else:
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
-  anova(args.values1, args.values2, args.values3, args.values4, args.values5)
+  anova(args.values1, args.values2, args.values3, args.values4, args.values5, args.values6, args.values7, out_fh=sys.stdout)
 
--- statx/correlation.py
+++ statx/correlation.py
@@ -11,43 +11,44 @@ import sys
 import numpy as np
 import scipy.stats
 
-def correlation(values1, values2):
-  logging.info('starting: %i values vs %i values', len(values1), len(values2))
-  logging.debug('group 1: %s; group 2: %s', ' '.join(values1), ' '.join(values2))
+def correlation(num1, num2, out_fh=None):
+  logging.info('starting: %i values vs %i values', len(num1), len(num2))
+  #logging.debug('group 1: %s; group 2: %s', ' '.join(values1), ' '.join(values2))
 
-  if len(values1) == 0 or len(values2) == 0:
+  if len(num1) == 0 or len(num2) == 0:
     logging.fatal('empty groups')
     return
 
-  num1 = [float(x) for x in values1]
-  num2 = [float(x) for x in values2]
-
-  sys.stdout.write('pearson\tp-value\tv1u\tv1sd\tv1min\tv1max\tv1n\tv2u\tv2sd\tv2min\tv2max\tv2n\n')
+  if out_fh is not None:
+    out_fh.write('pearson\tp-value\tv1u\tv1sd\tv1min\tv1max\tv1n\tv2u\tv2sd\tv2min\tv2max\tv2n\n')
 
   if all([num1[0] == x for x in num1 + num2]):
     logging.info('all values equal')
-    sys.stdout.write('0\t1\t{}\t0\t{}\t{}\t{}\t{}\t0\t{}\t{}\t{}\n'.format(num1[0], num1[0], num1[0], len(num1), num1[0], num1[0], num1[0], len(num2)))
-    return
+    if out_fh is not None:
+      out_fh.write('0\t1\t{}\t0\t{}\t{}\t{}\t{}\t0\t{}\t{}\t{}\n'.format(num1[0], num1[0], num1[0], len(num1), num1[0], num1[0], num1[0], len(num2)))
+    return {'r': 0, 'pvalue': 1}
 
   result = scipy.stats.pearsonr(num1, num2)
 
-  sys.stdout.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(result[0], result[1], np.mean(num1), np.std(num1), min(num1), max(num1), len(num1), np.mean(num2), np.std(num2), min(num2), max(num2), len(num2)))
+  if out_fh is not None:
+    out_fh.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(result[0], result[1], np.mean(num1), np.std(num1), min(num1), max(num1), len(num1), np.mean(num2), np.std(num2), min(num2), max(num2), len(num2)))
 
   logging.info('done')
+  return {'r': result[0], 'pvalue': result[1]}
 
-def read_csv(fh, col1, col2):
+def read_csv(fh, col1, col2, out_fh=None):
   logging.debug('reading stdin...')
   values1 = []
   values2 = []
   for row in fh:
-    values1.append(row[col1])
-    values2.append(row[col2])
-  correlation(values1, values2)
+    values1.append(float(row[col1]))
+    values2.append(float(row[col2]))
+  return correlation(values1, values2, out_fh)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Measure correlation')
-  parser.add_argument('--values1', required=False, nargs='+', help='group 1')
-  parser.add_argument('--values2', required=False, nargs='+', help='group 2')
+  parser.add_argument('--values1', required=False, nargs='+', type=float, help='group 1')
+  parser.add_argument('--values2', required=False, nargs='+', type=float, help='group 2')
   parser.add_argument('--col1', required=False, help='stdin table')
   parser.add_argument('--col2', required=False, help='stdin table')
   parser.add_argument('--delimiter', required=False, default='\t', help='stdin table')
@@ -59,6 +60,6 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
   if args.values1 is not None and args.values2 is not None:
-    correlation(args.values1, args.values2)
+    correlation(args.values1, args.values2, sys.stdout)
   else:
-    read_csv(csv.DictReader(sys.stdin, delimiter=args.delimiter), args.col1, args.col2)
+    read_csv(csv.DictReader(sys.stdin, delimiter=args.delimiter), args.col1, args.col2, sys.stdout)
